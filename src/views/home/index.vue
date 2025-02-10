378507950e608d4ef6fb9d9b1e382c532e9affcc
qwertyjiooo <143780130+qwertyjiooo@users.noreply.github.com>
2025-02-10 16:27:39 +0800

    111

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -11,6 +11,10 @@
 <script setup>
 // import { useBattery } from '@vueuse/core'
 // const { charging, chargingTime, dischargingTime, level } = useBattery()
+import axios from 'axios'
+// axios.get('http://192.168.1.28:4000/api/user/json').then(res => {
+//   console.log(res)
+// })
 </script>
 
 <style lang="less" scoped>
