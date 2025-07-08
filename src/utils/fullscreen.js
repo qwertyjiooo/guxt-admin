e66a530f1622e07cb5374c0019c85dfde8c3026b
yuxin <143780130+qwertyjiooo@users.noreply.github.com>
2025-07-08 17:50:51 +0800

    代码提交

--- src/utils/fullscreen.js
+++ src/utils/fullscreen.js
@@ -1,27 +1,10 @@
 // 控制全屏 的方法
-export const toggleFullscreen = () => {
-    const isFullscreen = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement;
-    if (isFullscreen) {
-        // 退出全屏
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        }
-    } else {
-        // 进入全屏
-        if (document.documentElement.requestFullscreen) {
-            document.documentElement.requestFullscreen();
-        } else if (document.documentElement.mozRequestFullScreen) {
-            document.documentElement.mozRequestFullScreen();
-        } else if (document.documentElement.webkitRequestFullscreen) {
-            document.documentElement.webkitRequestFullscreen();
-        } else if (document.documentElement.msRequestFullscreen) {
-            document.documentElement.msRequestFullscreen();
-        }
-    }
+export const toggleFullscreen = async () => {
+    // 检查当前是否处于全屏状态
+    const isFullscreen = document.fullscreenElement !== null;
+    // 退出全屏
+    if (isFullscreen) if (document.exitFullscreen) await document.exitFullscreen();
+    // 进入全屏
+    else if (document.documentElement.requestFullscreen) await document.documentElement.requestFullscreen();
 };
+
